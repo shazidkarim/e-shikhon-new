6a9d0e3b3e4d86f48c83eaa06f8b62cd38e8e5b5
shazidkarim <shazid747@gmail.com>
2024-02-20 12:21:36 +0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+import React, { FC } from "react";
+// import Heading from "../utils/Heading";
+// import AdminSidebar from "../component/Admin/Sidebar/AdminSidebar";
+// import AdminProtected from "../hooks/adminProtected";
+// import DashboardHero from "../component/Admin/DashboardHero";
+
+type props = {};
+
+const Page: FC<props> = () => {
+  return (
+    <div>
+      {/* <AdminProtected>
+        <Heading
+          title={`e-Shikhon - Admin`}
+          description="Learning"
+          keywords="React,Fron end ,"
+        /> */}
+
+        <div className="flex h-[200vh] ">
+          <div className="1500px:w-[16%] w-1/2">
+            {/* <AdminSidebar /> */}
+          </div>
+          <div className="w[85%]">
+            {/* <DashboardHero /> */}
+          </div>
+        </div>
+      {/* </AdminProtected> */}
+    </div>
+  );
+};
+
+export default Page;
